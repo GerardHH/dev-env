28432c029c2f603b12fe7f6833ccf77b7e050152
Gerard Heshusius <gHeshusius@lely.com>
2023-10-07 15:27:48 +0200

    zsh: Add language exports

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,3 +3,9 @@ HISTSIZE=1000000
 SAVEHIST=1000000
 export EDITOR="nvim"
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
+#
+# Setting for the UTF-8 terminal support
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
