a329e5af410b65d838236bb0e339ef4e223bc422
Gerard Heshusius <gHeshusius@lely.com>
2023-10-08 16:25:55 +0200

    zsh: aliases.zsh: Remove some unused aliasses

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,11 +3,6 @@
 alias lg='lazygit'
 alias vi='nvim'
 
-# confirm before overwriting something
-alias cp="cp -i"
-alias mv='mv -i'
-alias rm='rm -i'
-
 # easier to read disk
 alias df='df -h'     # human-readable sizes
 alias free='free -m' # show sizes in MB
@@ -16,4 +11,3 @@ alias free='free -m' # show sizes in MB
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
-alias ls='ls --color=auto'
