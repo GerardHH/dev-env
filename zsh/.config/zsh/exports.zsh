5479c432ae5a88b3a1ff672fec1707cd3c0db067
Gerard Heshusius <gHeshusius@lely.com>
2023-10-11 09:17:06 +0200

    zsh: Share history over all sesions

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,9 +1,21 @@
 #!/bin/sh
-HISTSIZE=1000000
-SAVEHIST=1000000
+
 export EDITOR="nvim"
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
-#
+
+# History
+# History file location + creation
+export HISTFILE="$HOME/.cache/zsh/zsh_history"
+[ ! -e $HISTFILE ] && mkdir -p "$(dirname $HISTFILE)" && touch "$HISTFILE"
+
+# History options
+HISTSIZE=100000
+SAVEHIST=$HISTSIZE
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_FIND_NO_DUPS
+
 # Setting for the UTF-8 terminal support
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
