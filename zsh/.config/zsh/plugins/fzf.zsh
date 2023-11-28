e507ddce1c801f3c1df759f987f93f8f7615e31f
Gerard Heshusius <gHeshusius@lely.com>
2023-11-28 16:35:51 +0100

    zsh: fzf powered cd and vi

--- zsh/.config/zsh/plugins/fzf.zsh
+++ zsh/.config/zsh/plugins/fzf.zsh
@@ -18,11 +18,22 @@ plug "joshskidmore/zsh-fzf-history-search"
 # #Default options to fzf command
 export FZF_DEFAULT_OPTS='--height 90% --layout=reverse --border --cycle'
 
+# Use fd to generate the list for file completion
 _fzf_compgen_path() {
     fd --hidden --follow --exclude ".git" . "$1"
 }
 
 # Use fd to generate the list for directory completion
 _fzf_compgen_dir() {
-    fd --type d --hidden --follow --exclude ".git" . "$1"
+    fd --type directory --hidden --follow --exclude ".git" . "$1"
+}
+
+# fzf powered cd command
+function fcd() {
+    cd $(fd --type directory --hidden --follow --exclude ".git" . "$1" | fzf --preview 'eza --tree --color=always --icons=always --all --level=1 {}')
+}
+
+# fzf powered vi command
+function fvi() {
+    vi $(fd --hidden --follow --exclude ".git" | fzf --preview 'bat {}')
 }
