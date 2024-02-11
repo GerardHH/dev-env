ce816eef33e9f257f415beb258560bd2e608c4c1
Gerard Heshusius <gHeshusius@lely.com>
2024-02-11 18:16:02 +0100

    awesome: Add catppuccin theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,7 +51,10 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+local chosen_theme = "catppuccin-mocha"
+local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme)
+beautiful.init(theme_path)
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
