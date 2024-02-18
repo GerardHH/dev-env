a94432becf1313b5889126167dd73a7f4faefc2e
Gerard Heshusius <gHeshusius@lely.com>
2024-02-18 17:44:10 +0100

    awesome: Remove not working catppuccin theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,10 +51,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
--- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-local chosen_theme = "catppuccin-mocha"
-local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme)
-beautiful.init(theme_path)
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
