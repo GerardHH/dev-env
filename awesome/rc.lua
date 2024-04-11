f229443013964052d05ab3c6f72006157aa51f9b
Gerard Heshusius <gHeshusius@lely.com>
2024-04-11 20:36:19 +0200

    awesome: rc: Remove wibar in favour of polybar

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -32,9 +32,29 @@ local menu = require("mod.menu")
 menubar.utils.terminal = definitions.terminal -- Set the terminal for applications that require it
 -- }}}
 
--- {{{ Wibar
-require("mod.status_bar")
--- }}}
+local function set_wallpaper(s)
+	-- Wallpaper
+	if beautiful.wallpaper then
+		local wallpaper = beautiful.wallpaper
+		-- If wallpaper is a function, call it with the screen
+		if type(wallpaper) == "function" then
+			wallpaper = wallpaper(s)
+		end
+		gears.wallpaper.maximized(wallpaper, s, true)
+	end
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
+
+-- Set up every screen
+awful.screen.connect_for_each_screen(function(s)
+	-- Wallpaper
+	set_wallpaper(s)
+
+	-- Each screen has its own tag table.
+	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
