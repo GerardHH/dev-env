9a4b23705bc22c9f7abf203922ccf6f3682b9022
Gerard Heshusius <gHeshusius@lely.com>
2024-04-12 08:50:09 +0200

    awesome: rules: Move border and focus rules to its own block and add polybar as an exception

--- awesome/mod/rules.lua
+++ awesome/mod/rules.lua
@@ -8,18 +8,23 @@ local clientbuttons = keys.clientbuttons
 
 -- Rules to apply to new clients (through the "manage" signal).
 awful.rules.rules = {
-	-- All clients will match this rule.
 	{
 		rule = {},
 		properties = {
-			border_width = beautiful.border_width,
-			border_color = beautiful.border_normal,
-			focus = awful.client.focus.filter,
 			raise = true,
 			keys = clientkeys,
 			buttons = clientbuttons,
 			screen = awful.screen.preferred,
 			placement = awful.placement.no_overlap + awful.placement.no_offscreen,
+		},
+	},
+	{
+		rule = {},
+		except_any = { class = { "Polybar" } },
+		properties = {
+			border_width = beautiful.border_width,
+			border_color = beautiful.border_normal,
+			focus = awful.client.focus.filter,
 		},
 	},
 }
