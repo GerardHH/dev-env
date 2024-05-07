b2a0d0011d0cc304f00196f4145b682dc61f6cc0
Gerard Heshusius <gHeshusius@lely.com>
2024-05-07 16:13:28 +0200

    yazi: Add the owner/group to the status bar

--- yazi/init.lua
+++ yazi/init.lua
@@ -41,3 +41,31 @@ function Status:name()
 	end
 	return ui.Span(" " .. h.name .. linked)
 end
+
+-- Return the owner/group of the selected item
+function Status:owner()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	})
+end
+
+-- Render the whole status bar
+function Status:render(area)
+	self.area = area
+
+	local left = ui.Line({ self:mode(), self:size(), self:name() })
+	local right = ui.Line({ self:owner(), self:permissions(), self:percentage(), self:position() })
+	return {
+		ui.Paragraph(area, { left }),
+		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+		table.unpack(Progress:render(area, right:width())),
+	}
+end
