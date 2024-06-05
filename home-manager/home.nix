cfae91998aeda30a4df6e4fc8284638f1bfd06cf
Gerard Heshusius <gh.heshusius@gmail.com>
2024-06-05 14:29:09 +0200

    home: Remove unused nixgl stuff

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,22 +1,8 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }: let
-  nixgl = import <nixgl> {};
-  nixGLWrap = pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-          wrapped_bin=$out/bin/$(basename $bin)
-          echo "exec ${lib.getExe' nixgl.auto.nixGLDefault "nixGL"} $bin \$@" > $wrapped_bin
-          chmod +x $wrapped_bin
-      done
-    '';
   HOME = builtins.getEnv "HOME";
   symlink = config.lib.file.mkOutOfStoreSymlink;
 in {
